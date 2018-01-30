38dbf1199821a4e96709b4e2f6797ed37b33b84d
Jonathan "Jono" Yang <jyang@nexb.com>
2018-01-30 15:51:05 -0800

    Ensure that the first and last ngrams are selected
    * Update docstring and help text
    
    Signed-off-by Jono Yang <jyang@nexb.com>

--- src/scancode/plugin_fingerprint.py
+++ src/scancode/plugin_fingerprint.py
@@ -30,8 +30,6 @@ from __future__ import unicode_literals
 from collections import OrderedDict
 
 from commoncode import filetype
-from licensedcode.tokenize import ngrams
-from licensedcode.tokenize import select_ngrams
 from plugincode.scan import ScanPlugin
 from plugincode.scan import scan_impl
 from scancode import CommandLineOption
@@ -41,14 +39,14 @@ from scancode import SCAN_GROUP
 @scan_impl
 class FingerprintScanner(ScanPlugin):
     """
-    Calculate the Halo Hash value of a Resource.
+    Calculate the Halo Hash and Hailstorm fingerprint value of a Resource.
     """
     sort_order = 4
 
     options = [
         CommandLineOption(('-g', '--fingerprints',),
             is_flag=True, default=False,
-            help='Calculate the Halo Hash value for <input>.',
+            help='Calculate the Halo Hash and Hailstorm fingerprint values for <input>.',
             help_group=SCAN_GROUP)
     ]
 
@@ -61,8 +59,11 @@ class FingerprintScanner(ScanPlugin):
 
 def get_fingerprint(location, **kwargs):
     """
-    Return a list of mappings for copyright detected in the file at `location`.
+    Return a list with a single OrderedDict that contains the bit average
+    Halo hash and Hailstorm fingerprint values.
     """
+    from licensedcode.tokenize import ngrams
+    from licensedcode.tokenize import select_ngrams
     from scancode.halohash import BitAverageHaloHash
 
     chunk_size = 1024
@@ -71,17 +72,41 @@ def get_fingerprint(location, **kwargs):
     if not filetype.is_file(location):
         return []
 
+    bah = BitAverageHaloHash()
     slices = []
     with open(location, 'rb') as f:
+        first_chunk = None
+        last_processed_chunk = None
         while True:
             chunk = f.read(chunk_size)
             if not chunk:
                 break
+
+            # We keep track of the first and last chunk to ensure that the
+            # first and last ngrams are selected
+            if not first_chunk:
+                first_chunk = chunk
+            last_processed_chunk = chunk
+
+            # Process data
+            bah.update(chunk)
             slices.extend(ngrams(chunk, ngram_length))
 
-    bah = BitAverageHaloHash()
-    for slice in select_ngrams(slices):
-        hashable = b''.join(slice)
-        bah.update(hashable)
+    selected_slices = list(select_ngrams(slices))
 
-    return [OrderedDict(bah128=bah.hexdigest())]
+    # Check to see if the first and last ngrams were selected
+    first_ngram = list(ngrams(first_chunk, ngram_length))[0]
+    last_ngram = list(ngrams(last_processed_chunk, ngram_length))[-1]
+    assert first_ngram in selected_slices
+    assert last_ngram in selected_slices
+
+    # Join slices together as a single bytestring
+    hashable = b''.join(b''.join(slice) for slice in selected_slices)
+    hailstorm = BitAverageHaloHash(hashable)
+
+    # Set values
+    fingerprints = OrderedDict()
+    fingerprints['bah128'] = bah.hexdigest()
+    fingerprints['hailstorm'] = hailstorm.hexdigest()
+
+    return [fingerprints]
